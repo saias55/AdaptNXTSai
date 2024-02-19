ed6e2e83663c2cf3d0f430bd7dc90e5612759b0b
saias55 <gudiguntasai77@gmail.com>
2024-02-19 11:18:35 +0000

    text details of adapt

--- src/components/OrderPending/index.js
+++ src/components/OrderPending/index.js
@@ -1,6 +1,8 @@
 import {FaArrowsAlt, FaSearch} from 'react-icons/fa'
+import {Component} from 'react'
 
 import OrderDetailsBox from '../OrderDetailsBox'
+import PageSection from '../PageSection'
 
 import './index.css'
 
@@ -18,7 +20,7 @@ const orderDetails = [
     orderNo: 2188888,
     orderDate: '20-2-2022',
     city: 'lucknow',
-    customer: 'sai kumar',
+    customer: 'ajay kumar',
     orderValue: '100',
   },
   {
@@ -26,73 +28,137 @@ const orderDetails = [
     orderNo: 210000,
     orderDate: '20-2-2022',
     city: 'lucknow',
-    customer: 'sai kumar',
+    customer: 'dooon kumar',
+    orderValue: '100',
+  },
+  {
+    id: 4,
+    orderNo: 210000,
+    orderDate: '20-2-2022',
+    city: 'lucknow',
+    customer: 'don',
+    orderValue: '100',
+  },
+  {
+    id: 5,
+    orderNo: 210000,
+    orderDate: '20-2-2022',
+    city: 'lucknow',
+    customer: 'vaishavi',
+    orderValue: '100',
+  },
+  {
+    id: 6,
+    orderNo: 210000,
+    orderDate: '20-2-2022',
+    city: 'lucknow',
+    customer: 'ragava',
     orderValue: '100',
   },
 ]
 
-const OrderPending = () => (
-  <div className="order-pending-bgc">
-    <div className="space-between">
-      <div>
-        <button type="button" className="order-pending-button-details">
-          Import Orders
-        </button>
-        <button type="button" className="order-pending-button-details">
-          Accept
-        </button>
-        <button type="button" className="order-pending-button-details">
-          Print
-        </button>
+class OrderPending extends Component {
+  state = {searchValue: ''}
+
+  onSearching = event => this.setState({searchValue: event.target.value})
+
+  // const filtered = orderDetails.filter(eachItem => eachItem.customer.toLowerCase().includes(searchValue.toLowerCase()))
+
+  render() {
+    const {searchValue} = this.state
+
+    const filtered = orderDetails.filter(eachItem =>
+      eachItem.customer.toLowerCase().includes(searchValue.toLowerCase()),
+    )
+    return (
+      <div className="order-pending-bgc">
+        <div className="space-between">
+          <div>
+            <button
+              type="button"
+              className="order-pending-button-details importButton"
+            >
+              Import
+            </button>
+
+            <button
+              type="button"
+              className="order-pending-button-details acceptButton"
+            >
+              Accept
+            </button>
+            <button
+              type="button"
+              className="order-pending-button-details printButton"
+            >
+              Print
+            </button>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="order-pending-button-details refreshButton"
+            >
+              Refresh
+            </button>
+          </div>
+        </div>
+        <br />
+        <input
+          type="text"
+          placeholder="search by customer name"
+          value={searchValue}
+          onChange={this.onSearching}
+          className="inputDetails"
+        />
+        <br />
+        <ul className="pending-unordered-details min-heigh">
+          <li className="li-items-flex">
+            <input type="checkbox" />
+            <div className="display-flex-1">
+              <p>Channel</p>
+              <FaArrowsAlt className="icon-details-size" />
+            </div>
+            <div className="display-flex-2">
+              <p>Order No</p>
+              <FaSearch className="icon-details-size" />
+            </div>
+            <div className="display-flex-3">
+              <p>Order Date</p>
+              <FaArrowsAlt className="icon-details-size" />
+            </div>
+            <div className="display-flex-4">
+              <p>City</p>
+              <FaArrowsAlt className="icon-details-size" />
+            </div>
+            <div className="display-flex-5">
+              <p>Customer Name</p>
+              <FaSearch className="icon-details-size" />
+            </div>
+            <div className="display-flex-6">
+              <p>Order Value</p>
+              <FaArrowsAlt className="icon-details-size" />
+            </div>
+            <div className="display-flex-7">
+              <p>Status</p>
+              <FaArrowsAlt className="icon-details-size" />
+            </div>
+            <div className="display-flex-8">
+              <p>Operation</p>
+            </div>
+          </li>
+          <hr />
+          {filtered.map(eachOrder => (
+            <OrderDetailsBox eachOrder={eachOrder} key={eachOrder.id} />
+          ))}
+        </ul>
+        <hr />
+        <div className="display-end">
+          <PageSection />
+        </div>
       </div>
-      <div>
-        <button type="button" className="order-pending-button-details">
-          Refresh
-        </button>
-      </div>
-    </div>
-    <ul className="pending-unordered-details min-heigh">
-      <li className="li-items-flex">
-        <input type="checkbox" />
-        <div className="display-flex-1">
-          <p>Channel</p>
-          <FaArrowsAlt className="icon-details-size" />
-        </div>
-        <div className="display-flex-2">
-          <p>Order No</p>
-          <FaSearch className="icon-details-size" />
-        </div>
-        <div className="display-flex-3">
-          <p>Order Date</p>
-          <FaArrowsAlt className="icon-details-size" />
-        </div>
-        <div className="display-flex-4">
-          <p>City</p>
-          <FaArrowsAlt className="icon-details-size" />
-        </div>
-        <div className="display-flex-5">
-          <p>Customer Name</p>
-          <FaSearch className="icon-details-size" />
-        </div>
-        <div className="display-flex-6">
-          <p>Order Value</p>
-          <FaArrowsAlt className="icon-details-size" />
-        </div>
-        <div className="display-flex-7">
-          <p>Status</p>
-          <FaArrowsAlt className="icon-details-size" />
-        </div>
-        <div className="display-flex-8">
-          <p>Operation</p>
-        </div>
-      </li>
-      <hr />
-      {orderDetails.map(eachOrder => (
-        <OrderDetailsBox eachOrder={eachOrder} key={eachOrder.id} />
-      ))}
-    </ul>
-    <hr />
-  </div>
-)
+    )
+  }
+}
 
 export default OrderPending
--- src/components/PageSection/index.js
+++ src/components/PageSection/index.js
@@ -1,9 +1,20 @@
-import {FaChevronLeft} from 'react-icons/fa'
+import {FaLongArrowAltLeft, FaLongArrowAltRight} from 'react-icons/fa'
+
+import './index.css'
 
 const PageSection = () => (
-  <div>
-    <button type="button">
-      <FaChevronLeft />
+  <div className="display-end">
+    <button type="button" className="pagePerMatterButton">
+      <FaLongArrowAltLeft />
+    </button>
+    <button type="button" className="pagePerMatterButton">
+      1
+    </button>
+    <button type="button" className="pagePerMatterButton">
+      <FaLongArrowAltRight />
+    </button>
+    <button type="button" className="matter-section-button">
+      20/page
     </button>
   </div>
 )
